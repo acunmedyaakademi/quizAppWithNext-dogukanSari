176f72cd723483be2f52cf8ce03e64e7a4ab2b30
Doğukan Sarı <teddogukan@gmail.com>
2024-10-03 14:50:47 +0300

    public klasörü oluşturuldu. İçerisine images adı altında klasör oluşturuldu ve fotoğraflar konuldu. Toggle.jsx'deki react den kalan img etiketleri Next.js'e göre düzenlendi.

--- app/components/Toggle.jsx
+++ app/components/Toggle.jsx
@@ -1,14 +1,39 @@
+import Image from 'next/image';
 export default function Toggle({ handleToggle, theme }) {
   return(
     <div className='toggle'>
-      <img className='lightsun' src="img/lightsun.svg" alt="light sun" />
-      <img className='darksun' src="img/darksun.svg" alt="dark sun" />
+      <Image
+        className='lightsun'
+        src="/images/lightsun.svg"
+        alt="Light Sun Logo"
+        width={76}
+        height={34}
+      />
+      <Image
+        className='darksun'
+        src="/images/darksun.svg"
+        alt="Dark Sun Logo"
+        width={76}
+        height={34}
+      />    
       <label className="switch">
         <input type="checkbox" onChange={handleToggle} checked={theme == 'dark' ? true : false} />
         <span className="slider round"></span>
       </label>
-      <img className='lightmoon' src="img/lightMoon.svg" alt="Light moon" />
-      <img className='darkMoon' src="img/darkMoon.svg" alt="dark moon" />
+      <Image
+        className='lightmoon'
+        src="/images/lightMoon.svg"
+        alt="Light Moon Logo"
+        width={76}
+        height={34}
+      />
+      <Image
+        className='darkMoon'
+        src="/images/darkMoon.svg"
+        alt="Light Moon Logo"
+        width={76}
+        height={34}
+      />
     </div>
   )
 }
