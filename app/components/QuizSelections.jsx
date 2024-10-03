f04193139d9c9072f513e6c25a6f13120f994fe4
Doğukan Sarı <teddogukan@gmail.com>
2024-10-03 14:59:57 +0300

    QuizSelection.jsx'deki react den kalan img etiketleri next.js e uyarlandı ve proje tamamlandı.

--- app/components/QuizSelections.jsx
+++ app/components/QuizSelections.jsx
@@ -1,10 +1,47 @@
+import Image from 'next/image';
+
 export default function QuizSelections({ handleQuizSelection }) {
   return(
     <div className="quizSelections">
-      <button onClick={() => handleQuizSelection('HTML')}> <img src="img/html.svg" alt="html imgesi" /> HTML</button>
-      <button onClick={() => handleQuizSelection('CSS')}> <img src="img/css.svg" alt="css imgesi" /> CSS</button>
-      <button onClick={() => handleQuizSelection('JAVASCRİPT')}> <img src="img/javaScript.svg" alt="javaScript imgesi" /> Javascript</button>
-      <button onClick={() => handleQuizSelection('ACCESSİBİLİTY')}> <img src="img/Accessibility.svg" alt="Accessibility imgesi" /> Accessibility</button>
+      <button onClick={() => handleQuizSelection('HTML')}>
+        <Image
+          src="/images/html.svg"
+          alt='html simgesi'
+          width={158}
+          height={28}
+        />
+        HTML
+      </button>
+
+      <button onClick={() => handleQuizSelection('CSS')}>
+        <Image
+          src="/images/css.svg"
+          alt='CSS simgesi'
+          width={158}
+          height={28}
+        /> 
+        CSS
+      </button>
+
+      <button onClick={() => handleQuizSelection('JAVASCRİPT')}>
+        <Image
+          src="/images/javaScript.svg"
+          alt='JavaScript simgesi'
+          width={158}
+          height={28}
+        />
+        Javascript
+      </button>
+
+      <button onClick={() => handleQuizSelection('ACCESSİBİLİTY')}>
+        <Image
+          src="/images/Accessibility.svg"
+          alt='Accessibility simgesi'
+          width={158}
+          height={28}
+        /> 
+        Accessibility
+      </button>
     </div>
   )
 }
